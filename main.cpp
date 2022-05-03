9c32a1d5a1ca918619c68fc28f147af8452a1a0d
lequoctrang4 <85050886+lequoctrang4@users.noreply.github.com>
2022-05-03 19:31:19 +0700

    Up File

--- main.cpp
+++ main.cpp
@@ -4,95 +4,14 @@
 #include <thread>
 #include <algorithm>
 #include <unistd.h>
+#include "Symaphore.h"
+#include "TaxiNotFoundException.h"
+#include "Taxi.h"
+#include "TaxiPool.h"
+#include "ClientThread.h"
 using namespace std;
 #define NUMBER_OF_CLIENT 8
-#define EXPIRED_TIME_IN_SECOND 1.2
-#define NUMBER_OF_TAXI 4
-class TaxiNotFoundException : public exception
-{
-    string mess;
-public:
-    TaxiNotFoundException(string instruction)
-    {
-        mess = instruction;
-    }
-    const char *what() const throw()
-    {
-        return mess.c_str();
-    }
-};
-class Taxi{
-private:
-    string name;
-public:
-    Taxi(const string& name):name(name){};
-    string getName() {
-        return name;
-    }
-    void setName(string name){
-        this->name = name;
-    }
-    string toString(){
-        return "Taxi [name=" + name + "]";
-    }
-    Taxi operator=(const Taxi& taxi){
-        return taxi;
-    }
-    bool operator==(const Taxi& taxi){
-        return (this->name == taxi.name);
-    }
-};
 
-class TaxiPool {
-private:
-    list<Taxi> available;
-    list<Taxi> inUse;
-    int count = 0;
-    bool waiting = false;
-public:
-    Taxi getTaxi() {
-        if (!available.empty()) {
-            Taxi taxi = available.front();
-            available.pop_front();
-            inUse.push_back(taxi);
-        }
-        if (count == NUMBER_OF_TAXI) {
-            this->waitingUntilTaxiAvailable();
-            return this->getTaxi();
-        }
-        Taxi taxi = this->createTaxi();
-        inUse.push_back(taxi);
-        return taxi;
-    }
-
-    void release(Taxi taxi) {
-        list<Taxi>::iterator it = inUse.begin();
-        for (; it != inUse.end() ; ++it) {
-            if(*it == taxi) break;
-        }
-        remove(inUse.begin(), inUse.end(), taxi);
-        available.push_back(taxi);
-        cout << taxi.getName() << " is free\n";
-    }
-
-    Taxi createTaxi() {
-        sleep(0.2); // The time to create the taxi.
-        Taxi taxi("Taxi " + to_string(count));
-        cout << taxi.getName() + " is created\n";
-        return taxi;
-    }
-
-    void waitingUntilTaxiAvailable() {
-        if (waiting) {
-            waiting = false;
-            throw TaxiNotFoundException("No taxi available");
-        }
-    }
-};
-
-class ClientThread{
-
-};
 int main(){
 
 }
